fb264e8e89620bdc6fd492e745d3ae7866013ca3
Filipe <filipe.silva@fiancarapida.com>
2023-08-20 22:36:37 -0300

    fix: adjust ci and schema for post

--- app/v1/schema/pessoas.js
+++ app/v1/schema/pessoas.js
@@ -2,7 +2,7 @@ const PessoasBody = j => j.object({
   apelido: j.string().max(23).required(),
   nome: j.string().max(100).required(),
   nascimento: j.date().required(),
-  stack: j.array().items(j.string().max(32)).required(),
+  stack: j.array().items(j.string().max(32)).allow(null).optional(),
 });
 
 const PessoasParams = j => j.object({
